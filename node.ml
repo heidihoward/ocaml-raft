5188b4f15d071ba2186a91094e0702bccc0ac25b
heidi-ann <hh360@cam.ac.uk>
2013-08-13 18:52:46 +0100

    end of another day

--- node.ml
+++ node.ml
@@ -2,7 +2,8 @@ open Core.Std
 open Async.Std
 open Msg
 
-(* type role = Follower | Candidate | Leader 
+(* _______ Raft Specific Code ______ *)
+type role = Follower | Candidate | Leader 
 
 type log_entry = 
   { term: int;
@@ -10,13 +11,57 @@ type log_entry =
     command: string; }
 
 type state = 
-  { candidate_id: int;
+  { mutable id: string;
+    ele_timeout: Time.Span.t;
+    mutable start_timeout: Time.Ofday.t; 
     mutable all_ids: int list;
     mutable leader_id: int option;
     mutable cnt_role: role;
     mutable cnt_term: int;
     mutable voted_for: int option;
-    mutable log: log_entry list } *)
+    mutable log: log_entry list } 
+
+let state = { id = "N/a"; (* this is always overwritten when run is called *)
+              ele_timeout = Time.Span.create ~sec:10 ();
+              start_timeout = Time.Ofday.now();
+              all_ids=[];
+	      leader_id= None;
+	      cnt_role = Follower;
+	      cnt_term = 0;
+	      voted_for = None;
+	      log = []; }
+
+
+(* runnning in follower role *)
+let rec run_follower pkt_r pkt_w log_w = 
+  let waketime = Time.Ofday.add state.start_timeout state.ele_timeout in
+  let date = Date.of_tm (Unix.gmtime (Unix.time ())) in
+  match waketime with 
+  | Some time -> 
+  Clock.at (Time.of_local_date_ofday date time) 
+  >>> (fun _ -> 
+    let time_passed = Time.Ofday.diff (Time.Ofday.now()) state.start_timeout in
+    if (state.cnt_role = Follower) then ( 
+      if (state.ele_timeout >=. time_passed) then (
+        debug "electon timeout exprired, switching to candidate mode"; 
+        run_candidate pkt_r pkt_w log_w )
+      else (
+        debug "electon timeout non-exprired, will check again later"; 
+        state.start_timeout <- Time.Ofday.now();
+	run_follower pkt_r pkt_w log_w ))
+    else debug "node no longer a follower so timer is not currently requried" )
+  | None -> debug "its midnight"
+
+
+let msg_rcv_raft msg = 
+(* TODO pharse packets *)
+  match (String.split msg ~on:':') with 
+  | (state.id)::"SIM"::txt::_ -> (* control msg from simulator *) return () 
+  | (state.id)::"CLIENT"::txt::_ -> (* process client request *) return ()
+  | (state.id)::from::"RVQ"
+  | (state.id)::from::"RQA"
+  | (state.id)::from::"APQ"
+  | (state.id)::from::"APA"
 
 (* let create_persistent state =
    let file = (string_of_int state.candidate_id)^"log.log" in
@@ -48,6 +93,7 @@ let clientrequest command:string =
   |Leader -> *)
 (*  >>= (fun _ -> create_persistent state) *)
 
+(* _____ General Code for simulator ______ *)
 
 let debug_active = ref true
 
@@ -55,39 +101,43 @@ let debug x = if !debug_active then
   let time = Time.Ofday.to_sec_string (Time.Ofday.now()) in
   (printf "[%s] %s \n %!" time x)
 
-
 let rec msg_rcv  r log_w = 
   Reader.read_line r 
   >>> (function 
     |`Ok msg ->  debug ("MSG incoming: "^(Msg.to_string msg)); Msg.append_log log_w msg; msg_rcv  r log_w 
     |`Eof -> debug "EoF has been read from socket"; msg_rcv r log_w ) 
 
-let msg_snd id pkt_w log_w too msg =
-  let msg_pkt = too^":"^(string_of_int id)^":"^msg in
+let msg_snd pkt_w log_w too msg =
+  let msg_pkt = too^":"^(state.id)^":"^msg in
   debug ("MSG outgoing: "^(Msg.to_string msg_pkt));
   Msg.append_log log_w msg_pkt; 
   Writer.write_line pkt_w msg_pkt
 
-let rec test_msgs id pkt_w log_w= 
+(* random traffic generator, used for testing *)
+let rec test_msgs pkt_w log_w= 
   let timephase = Time.Span.randomize (Time.Span.create ~sec:3 ()) ~percent:0.75 in 
   let rcv = string_of_int (Random.int 10) in
   Clock.after timephase
   >>> (fun _ -> 
-  msg_snd id pkt_w log_w rcv "random hello msg"; 
-  test_msgs id pkt_w log_w )
+  if (not (phys_equal rcv state.id)) then (
+  msg_snd pkt_w log_w rcv "random hello msg" );
+  test_msgs pkt_w log_w )
 
 let run ~id ~port = 
   debug "starting up";
   Random.self_init ();
+  state.id <- (string_of_int id);
   Tcp.connect (Tcp.to_host_and_port "localhost" port)
   >>| (fun (_,pkt_r,pkt_w) ->  
     debug "connected to simulator";
-    let log_w = Msg.msg_log (string_of_int id) in
+    let log_w = Msg.msg_log (state.id) in
     msg_rcv pkt_r log_w;  
-    msg_snd id pkt_w log_w "SIM" "hello";
-    test_msgs id pkt_w log_w )
+    msg_snd pkt_w log_w "SIM" "hello";
+    run_raft pkt_r pkt_w log_w;
+    test_msgs pkt_w log_w )
   >>= (fun _ ->  Deferred.never () )
- 
+
+(*command line parsing & starting async schedular *) 
 let () =
   Command.async_basic
     ~summary:"Start an network node"
@@ -95,8 +145,8 @@ let () =
       empty
       +> flag "-id" (required int)
         ~doc:" candidate ID"
-      +> flag "-port" (optional_with_default 8889 int)
-        ~doc:" Port to listen on (default 8889)"
+      +> flag "-port" (optional_with_default 8888 int)
+        ~doc:" Port to listen on (default 8888)"
     )
     (fun id port () -> run ~id ~port)
   |> Command.run
