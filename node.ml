8609f79434ca85d9269dd004e977da952958b7cc
heidi-ann <hh360@cam.ac.uk>
2013-09-06 17:04:35 +0100

    sdf

--- node.ml
+++ node.ml
@@ -1,24 +1,28 @@
 open Core.Std
 open Async.Std
 
+(*Specification for the RPC method called myrpc *)
 let myrpc_spec = Rpc.Rpc.create
   ~name:"my-little-rpc"
   ~version:0
   ~bin_query:String.bin_t
   ~bin_response:String.bin_t
 
-let myrpc_impl () = printf "client hello"; return "hello World"
+(*Implementation for the RPC method called myrpc *)
+let myrpc_impl () x = printf x; return "hello World"
 
+(* Connect to TCP server at port [peer] and dispatch myrpc request *)
 let start_client peer =
   Tcp.with_connection (Tcp.to_host_and_port "127.0.0.1" peer) (fun _ r w ->
-    Rpc.Connection.create ~connection_state:() r w  
+    Rpc.Connection.create ~connection_state:() r w
     >>= function
     | Error exn -> raise exn
-    | Ok conn -> Rpc.Rpc.dispatch myrpc_spec conn  )
+    | Ok conn -> Rpc.Rpc.dispatch myrpc_spec conn "good morning jon" )
 
+(*Start a RPC server on [port] *)
 let start_server ~port =
   let implementations = Rpc.Implementations.create 
-    ~implementations:[Rpc.Rpc.implement myrpc_spec () myrpc_impl () ] 
+    ~implementations:[Rpc.Rpc.implement myrpc_spec myrpc_impl ] 
     ~on_unknown_rpc: `Ignore in
   Rpc.connection.serve ~implementations ~initial_connection_state:() (Tcp.on_port port) 
 
