e8c159b98ca7b70093f353f66128ac04fea5d585
heidi-ann <hh360@cam.ac.uk>
2013-09-09 13:06:35 +0100

    it works, this is an example of some peers doing simple RPC

--- node.ml
+++ node.ml
@@ -9,27 +9,31 @@ let myrpc_spec = Rpc.Rpc.create
   ~bin_response:String.bin_t
 
 (*Implementation for the RPC method called myrpc *)
-let myrpc_impl () x = printf x; return "hello World"
+let myrpc_impl () x = printf "%s"x; return "hello World"
 
 (* Connect to TCP server at port [peer] and dispatch myrpc request *)
 let start_client peer =
-  Tcp.with_connection (Tcp.to_host_and_port "127.0.0.1" peer) (fun _ r w ->
+  ignore (
+    Tcp.with_connection (Tcp.to_host_and_port "127.0.0.1" peer) (fun _ r w ->
     Rpc.Connection.create ~connection_state:() r w
     >>= function
     | Error exn -> raise exn
-    | Ok conn -> Rpc.Rpc.dispatch myrpc_spec conn "good morning jon" )
+    | Ok conn -> Rpc.Rpc.dispatch myrpc_spec conn "good morning jon"))
 
 (*Start a RPC server on [port] *)
 let start_server ~port =
-  let implementations = Rpc.Implementations.create 
+  let implementations = Rpc.Implementations.create_exn 
     ~implementations:[Rpc.Rpc.implement myrpc_spec myrpc_impl ] 
     ~on_unknown_rpc: `Ignore in
-  Rpc.connection.serve ~implementations ~initial_connection_state:() (Tcp.on_port port) 
+  Rpc.Connection.serve ~implementations
+  ~initial_connection_state:(fun _ -> ()) ~where_to_listen:(Tcp.on_port port) ()
 
 let run ~port ~peers = 
   printf "hello";
-  start_server ~port;
-  List.iteri peers start_client 
+  List.iter peers start_client;
+  ignore (start_server ~port);
+  Deferred.never()
+
 
 let () =
   Command.async_basic
@@ -38,9 +42,8 @@ let () =
       empty
       +> flag "-port" (optional_with_default 8888 int)
         ~doc:" Port to listen on (default 8888)"
-      +> flag "-peers" (requried listed int)
+      +> flag "-peers" (listed int)
         ~doc:" Peers to say hello to"
-
     )
     (fun port peers () -> run ~port ~peers)
   |> Command.run
