f90aadd623150a323932d1088f827bf60f6b453e
heidi-ann <hh360@cam.ac.uk>
2013-10-30 19:22:41 +0000

    baby steps

---
+++ main.ml
@@ -0,0 +1,21 @@
+open Core.Std
+
+
+module State = struct
+  type t = 
+  type statecall
+
+  let init () = 
+    ...
+  let print s :state -> unit =
+
+  let tick s tk : t -> statecall -> t =
+
+
+
+end
+
+
+let main = 
+  init() 
+  
