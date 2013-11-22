de1d72762e9cc4baad2650651f2d5a7e8f5e8970
heidi-ann <hh360@cam.ac.uk>
2013-11-22 15:23:37 +0000

    new version without mutable stategit add main.ml

--- main.ml
+++ main.ml
@@ -103,51 +103,44 @@ let timeout = MonoTime.create 5
 
 (*state is the global information, readable by all and only modified by
  * statecalls via State.tick, the mutablity here should be handled better *)
-let state = ref State.init
 
 let debug_active = ref true
 
 let debug x = if !debug_active then 
-  let time = MonoTime.print !state.time in
-  (printf "[%s] %s \n" time x)
-
-(* wrappers around ugly mutable state *)
-let update tk =  state := State.tick !state tk
+  (printf " %s \n"  x)
 
 (* ----- all the events, can probably be moved into own module -----*)
 
 (* type event =  E: unit -> (MonoTime.t * event) list *)
-type 'a e = Next of ('a * (unit ->  'a e)) list
+type 'a e = Next of ('a *(State.t -> State.t * 'a e)) list
 
-let rec incrTime () = update IncrementTime; Next []
+let rec incrTime s = (State.tick s IncrementTime, Next [])
 
-and startCand () = debug "Entering Candidate Mode";
-  update IncrementTerm;
-  update (Vote !state.id); 
-  let reqs = List.map !state.allNodes 
-    ~f:(fun rcv -> (!state.time, requestVoteRq !state.term !state.id !state.lastlogIndex
-    !state.lastlogTerm rcv)) in
-  let t = MonoTime.add !state.time timeout in
-  Next ((t, checkTimer)::reqs)
+and startCand s = debug "Entering Candidate Mode";
+  let snew = State.tick (State.tick s IncrementTerm) (Vote s.id ) in
+  let reqs = List.map snew.allNodes 
+    ~f:(fun rcv -> (snew.time, requestVoteRq snew.term snew.id snew.lastlogIndex
+    snew.lastlogTerm rcv)) in
+  let t = MonoTime.add snew.time timeout in
+  (snew, Next ((t, checkTimer)::reqs))
 
-and checkTimer () = debug "Checking heartbeat timer"; 
-  if (!state.mode = Follower) then
+and checkTimer s = debug "Checking heartbeat timer"; 
+  if (s.mode = Follower) then
     (* if heartbeat is true, we have rec a packet in the last election timeout*)
-    if !state.heartbeat then 
-      let t = MonoTime.add !state.time timeout in
-      update Reset;
-      Next [(t, checkTimer )] 
+    if s.heartbeat then 
+      let t = MonoTime.add s.time timeout in
+      (State.tick s Reset, Next [(t, checkTimer )]) 
     (* we have timedout so become candidate *)
-    else Next [(!state.time, startCand)]
-  else Next []
+    else (s,Next [(s.time, startCand)])
+  else (s,Next [])
 
-and startFollow () = debug "Entering Follower mode";
-  let t = MonoTime.add !state.time timeout in
-  Next [(t, checkTimer)]
+and startFollow (s:State.t) = debug "Entering Follower mode";
+  let t = MonoTime.add s.time timeout in
+  (s,Next [(t, checkTimer)])
 
-and requestVoteRq term cand_id lst_index last_term rvc () =
+and requestVoteRq term cand_id lst_index last_term rvc s =
   debug ("Dispatch request to"^ ID.print rvc );
-  Next []
+  (s,Next [])
 
   (*
 let RequestVoteRs
@@ -160,21 +153,22 @@ let AppendEntriesRs
 let eventlist = Next [(MonoTime.init(), startFollow);
                  (MonoTime.create 30, incrTime)]
 
-let rec run = function 
+let rec run (s:State.t) = function 
   | Next events -> 
-  let f (t,_) = MonoTime.comp !state.time t in 
+  let f (t,_) = MonoTime.comp s.time t in 
   match (List.partition_tf events ~f) with
   | ([],[]) -> debug "finished all events"
     (* if no more events for this time then increment time and try again *)
-  | ([],_) ->  ignore(incrTime ()); run (Next events)
+  | ([],_) ->  let (snew,_) = incrTime s in
+     run snew (Next events)
     (* if there are event to be executed, run them *)
-  | (now,later) -> 
-      let results = List.map now ~f:(fun (t,e) -> match e () with Next x -> x) in
+  | (next::thn,later) -> 
+      let snew,enew = (match next with (_,e) -> e s) in
+      let enew = (match enew with Next x -> x) in
     (* will return any new events to add to event list so add to list*)
-  run (Next (later@(List.concat results)))
+  run snew (Next (enew @ thn @ later)) 
 
-let main = 
-  run(eventlist)
+let main = run State.init eventlist 
 
 
   
