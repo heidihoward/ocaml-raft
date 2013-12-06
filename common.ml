aba3cff380acd8602d51411f802aa3111e002cb8
heidi-ann <hh360@cam.ac.uk>
2013-12-06 18:37:18 +0000

    modifying DES for multiple nodes

--- common.ml
+++ common.ml
@@ -22,19 +22,31 @@ end
 
 module type ID = sig
   type t
+(*  type loc*)
+  type msg
   val from_int: int -> t
-  val to_int: t -> t
+  val to_int: t -> int
   val comp: t -> t -> bool
   val to_string: t -> string
+(*  val get_loc: t -> loc *)
+(*  val set_loc: t -> loc -> t *)
+  val dispatch: t -> msg -> t
+  val collect: t -> msg list
 end 
 
 module IntID : ID  = struct
   type t = int
+(*  type loc = unit *)
+  type msg = unit
   let from_int x = x
   let to_int x  = x
   let comp t1 t2 = phys_equal t1 t2
   let to_string = string_of_int
-end
+(*  let get_loc _ = None *)
+(*  let set_loc t _ = t *)(* simple IntID don't hold any location information *) 
+  let dispatch x _ = x
+  let collect _ = []
+end  
 
 module type ENTRY = sig 
   type t 
@@ -64,6 +76,49 @@ module ListLog : LOG =
   let init () = []
   let append t x = x::t
   let to_string = List.to_string ~f:LogEntry.to_string
+end
+
+module Event = struct 
+  type ('a,'b) t = E of ('a * ('a,'b) event)
+  and ('a,'b) event = ('b -> ('b * ('a,'b) t list))
+
+  let compare x y = match x,y with
+  | (E (xt,xe),E (yt,ye)) -> compare xt yt
+end 
+
+module type EVENTLIST = sig
+  type ('a,'b) t
+  val from_list: ('a,'b) Event.t list -> ('a,'b) t
+  val to_list: ('a,'b) t -> ('a,'b) Event.t list
+  val find: 'a -> ('a,'b) t -> ('b * ('a,'b) t ) option
+  val add: ('a,'b) Event.t -> ('a,'b) t -> ('a,'b) t
+end
+
+module EventList = struct
+
+  type ('a,'b) t = ('a,'b) Event.t  list
+
+  let from_list x = List.sort ~cmp:Event.compare x
+  let to_list x = x
+
+(*  let find x l =  
+    let open Event in
+    let f = function | E (t,e) -> (x=t) in
+    (* TODO write better implementation of find which returns list *)
+    match (List.partition_tf l ~f) with
+  | (E (_,e)::_,ls) -> Some (e,ls)
+  | ([],_) -> None  *)
+
+  let find t l = 
+    (* TODO ask anil why does Event.(..) work here ? *)
+    let open Event in 
+    match l with
+    | E (lt,le)::ls -> if lt=t then Some (le,ls) else None
+    | _ -> None 
+
+
+  let add a l = 
+    List.merge l (from_list a) ~cmp:Event.compare
 end
 
 
--- des.ml
+++ des.ml
@@ -6,52 +6,29 @@ open Env
 module MonoTime = Clock.FakeTime
 module State = Env.PureState(IntID)(MonoTime)(Index)(LogEntry)(ListLog)
 
-module Event = struct 
-  type ('a,'b) t = E of ('a * ('a,'b) event)
-  and ('a,'b) event = ('b -> ('b * ('a,'b) t list))
-
-  let compare x y = match x,y with
-  | (E (xt,xe),E (yt,ye)) -> compare xt yt
+module SimID : ID = struct
+  (* type loc = (MonoTime.t,State.t) EventList.t option *)
+  (* TODO ask anil about the get_loc type issue for tuples *)
+  type msg = (MonoTime.t,State.t) Event.t
+  type t = int *(msg list)
+  let from_int x = (x,[])
+  let to_int = fst
+  let comp (t1,_) (t2,_) = (t1=t2)
+  let to_string (x,_) = string_of_int x
+(*  let get_loc = snd *)
+(*  let set_loc (x,_) l = (x,l) *)
+  let dispatch (x,msgs) msg = (x,msg::msgs)
+  let collect = snd
 end 
-
-module type EVENTLIST = sig
-  type ('a,'b) t
-  val from_list: ('a,'b) Event.t list -> ('a,'b) t
-  val to_list: ('a,'b) t -> ('a,'b) Event.t list
-  val find: 'a -> ('a,'b) t -> ('b * ('a,'b) t ) option
-  val add: ('a,'b) Event.t -> ('a,'b) t -> ('a,'b) t
-end
-  
-
-module EventList = struct
-
-  type ('a,'b) t = ('a,'b) Event.t  list
-
-  let from_list x = List.sort ~cmp:Event.compare x
-  let to_list x = x
-
-  let find x l =  
-    let open Event in
-    let f = function | E (t,e) -> (x=t) in
-    (* TODO write better implementation of find which returns list *)
-    match (List.partition_tf l ~f) with
-  | (E (_,e)::_,ls) -> Some (e,ls)
-  | ([],_) -> None  
-
-  let find t l = 
-    (* TODO ask anil why does Event.(..) work here ? *)
-    let open Event in 
-    match l with
-    | E (lt,le)::ls -> if lt=t then Some (le,ls) else None
-    | _ -> None 
-
-
-  let add a l = 
-    List.merge l (from_list a) ~cmp:Event.compare
-end
 
 open EventList
 open Event
+
+
+module DEventSim = functor (Id:ID) -> struct
+
+module MonoTime = Clock.FakeTime
+module State = Env.PureState(Id)(MonoTime)(Index)(LogEntry)(ListLog)
 
 let timeout = MonoTime.span_of_int 5
 
@@ -85,12 +62,12 @@ and startFollow (s:State.t)  = debug "Entering Follower mode";
 
   (* TODO ask anil why s needs to explicitly annotated to access its field *)
 and requestVoteRq term cand_id lst_index last_term rvc (s:State.t) =
-  debug ("Dispatch request to "^ IntID.to_string rvc );
+  debug ("Dispatch request to "^ Id.to_string rvc );
   (* Simulated Responses *)
   (s,[E (MonoTime.succ s.time, requestVoteRs term true rvc )])
   
 and requestVoteRs term voteGranted id (s:State.t) = 
-  debug ("Receive request reply from "^ IntID.to_string id );
+  debug ("Receive request reply from "^ Id.to_string id );
   if (term > s.term) 
   then (State.tick (StepDown term) s,[ E (s.time,startFollow)]) 
   else if (voteGranted) 
@@ -103,19 +80,51 @@ and checkElection c s =
   | Timeout -> (*TODO: check electon outcome *) (s,[])
   | Vote -> (* TODO:check election outcome *) (s,[])
 
-let eventlist =  [E (MonoTime.init(), startFollow);
-                  E (MonoTime.t_of_int 30, incrTime)]
 
-let rec run ?term (s:State.t) (el: (MonoTime.t,State.t) EventList.t)  = 
-  match el with | [] -> debug "terminating" 
-  | l -> debug (MonoTime.to_string s.time);
+let rec run ~term (s:State.t) (el: (MonoTime.t,State.t) EventList.t)  = 
+  (* checking for termination conditions *)
+  match el with 
+  | []-> debug "terminating as no events remain" 
+  | l -> ( 
+    if ( match term with | Some tt -> (tt=s.time) | _ -> false )
+      then debug "terminating as terminate time has been reached"
+      else (
     match (EventList.find s.time l) with
     | Some (e,ls) -> 
         let s_new,e_new = e s in
         State.print s_new;
-        run s_new (EventList.add e_new ls)
+        run ~term s_new (EventList.add e_new ls)
     | None -> 
         debug "Incrementing Time"; State.print s;
-        run (State.tick IncrementTime s) el
+        run ~term (State.tick IncrementTime s) el )) 
 
-let main = run (State.init()) (EventList.from_list eventlist)
+let run_multi l: (State.t * (MonoTime.t,State.t) EventList.t) list =
+  let f ((s,el):(State.t * (MonoTime.t,State.t) EventList.t)) = 
+    run  ~term:(Some (MonoTime.succ s.time)) s el in
+  (* run each node for one time unit *)
+  let l_new =  List.map ~f l in 
+  (* collect events across the nodes *)
+  let all_ids = List.map ~f:(fun (s,_) ->  s.allNodes) l_new 
+             |> Caml.List.flatten in
+  let msgs n = List.map ~f:(fun node -> if (Id.to_int node = n) then Id.collect
+              else []) |> Caml.List.flatten in
+  List.map ~f:(fun n (s,el) -> (s,EventList.add el (msgs n))) l_new
+  |> run_multi 
+ 
+
+
+end
+
+module DES =  DEventSim(IntID)
+module DESmulti = DEventSim(SimID)
+
+let eventlist = DES.( [E (MonoTime.init(), startFollow);
+                  E (MonoTime.t_of_int 30, incrTime)] )
+
+let main = 
+  DES.run 
+  ~term:(Some (MonoTime.t_of_int 5)) 
+  (DES.State.init()) 
+  (EventList.from_list eventlist)
+
+ (* DESmulti.run_multi *)
