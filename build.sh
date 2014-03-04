5a11aeb758b0e2abcc890b8a5077e1f021f25a2a
Heidi-ann <hh360@cam.ac.uk>
2014-03-04 10:39:47 +0000

    moving code in lib directory

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+cd lib
+
 rm _build/*
 rm *.{byte,native}
 
--- test1.sh
+++ test1.sh
@@ -1,2 +1,2 @@
-./cmdstart.byte discrete -nodes 4 -d -follower Uniform-150-300 -candidate Fixed-50 -leader Fixed-50 -delay Fixed-6 -failure Uniform-10000-20000 -recover Fixed-10.0
+./lib/cmdstart.byte discrete -nodes 4 -d -follower Uniform-150-300 -candidate Fixed-50 -leader Fixed-50 -delay Fixed-6 -failure Uniform-10000-20000 -recover Fixed-10.0
 
