0d1dd7b7a8717e89b5a821267a421379ac72eb43
Heidi-ann <hh360@cam.ac.uk>
2014-03-20 15:16:24 +0000

    adding sample workloads

--- travis.sh
+++ travis.sh
@@ -12,3 +12,8 @@ eval `opam config env`
 # build ocaml-raft
 ./build.sh
 
+
+# testing ocaml-raft
+./cmdstart.byte discrete -termOnClient -nodes 2 -d -follower Uniform-150-300 -candidate Fixed-50 -leader Fixed-50 -delay Fixed-6
+./cmdstart.byte discrete -termOnClient -nodes 2 -d -follower Uniform-150-300 -candidate Fixed-50 -leader Fixed-50 -delay Fixed-6 -failure Uniform-400-500 -recover Uniform-5-10
+
