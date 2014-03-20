aa80abdb55dfb5e5c569537e3b03048082326a8c
Heidi-ann <hh360@cam.ac.uk>
2014-03-20 17:46:34 +0000

    making the SPL build local

--- travis.sh
+++ travis.sh
@@ -2,18 +2,18 @@
 ppa=avsm/ocaml41+opam11
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam 
 
 export OPAMYES=1
 opam init
-opam install core async
+opam install core async ounit
 eval `opam config env`
 
 # spl
-git clone https://github.com/avsm/melange.git
-cd melange/tools/spl
-make all
-cd ~
+# git clone https://github.com/avsm/melange.git
+# cd melange/tools/spl
+# make all
+# cd ~
 
 # build ocaml-raft
 ./build.sh
