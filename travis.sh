d7f60e3c707470693b3817ef69a6fd5012dd1fd4
Heidi-ann <hh360@cam.ac.uk>
2014-03-20 17:36:04 +0000

    adding SPL unit testing

--- travis.sh
+++ travis.sh
@@ -9,6 +9,12 @@ opam init
 opam install core async
 eval `opam config env`
 
+# spl
+git clone https://github.com/avsm/melange.git
+cd melange/tools/spl
+make all
+cd ~
+
 # build ocaml-raft
 ./build.sh
 
@@ -16,4 +22,9 @@ eval `opam config env`
 # testing ocaml-raft
 ./cmdstart.byte discrete -termOnClient -nodes 2 -d -follower Uniform-150-300 -candidate Fixed-50 -leader Fixed-50 -delay Fixed-6
 ./cmdstart.byte discrete -termOnClient -nodes 2 -d -follower Uniform-150-300 -candidate Fixed-50 -leader Fixed-50 -delay Fixed-6 -failure Uniform-400-500 -recover Uniform-5-10
+
+# unit testing
+./test_splaytree.byte
+./test_spl.byte
+
 
