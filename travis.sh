346a56e92884278687eb1d5768db0200cc0e904e
Heidi-ann <hh360@cam.ac.uk>
2014-03-20 13:28:21 +0000

    adding travis files

---
+++ travis.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+ppa=avsm/ocaml41+opam11
+echo "yes" | sudo add-apt-repository ppa:$ppa
+sudo apt-get update -qq
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+
+export OPAMYES=1
+opam init
+opam install core async
+eval `opam config env`
+
+./build.sh
