31b81911c9b1a24bc75b66de118672cc0a23c84b
Heidi-ann <hh360@cam.ac.uk>
2014-04-23 10:12:05 +0100

    expanding travis build matrix

--- travis.sh
+++ travis.sh
@@ -20,12 +20,12 @@ eval `opam config env`
 
 
 # testing ocaml-raft
-./cmdstart.byte discrete -termOnClient -nodes 2 -d -follower Uniform-150-300 -candidate Fixed-50 -leader Fixed-50 -delay Fixed-6
-./cmdstart.byte discrete -termOnClient -nodes 2 -d -follower Uniform-150-300 -candidate Fixed-50 -leader Fixed-50 -delay Fixed-6 -failure Uniform-400-500 -recover Uniform-5-10
+./cmdstart.byte discrete -termOnClient -nodes 2 -follower Uniform-150-300 -candidate Fixed-50 -leader Fixed-50 -delay Fixed-6 
+./cmdstart.byte discrete -termOnClient -nodes 2 -follower Uniform-150-300 -candidate Fixed-50 -leader Fixed-50 -delay Fixed-6 -failure Uniform-400-500 -recover Uniform-5-10
 ./cmdstart.byte discrete -nodes 30  -follower Uniform-150-155 -candidate Uniform-11-22 -leader Fixed-75 -delay Fixed-7 -termOnTimeout 50000 -termOnElec -cmds 0 -backoff
-./cmdstart.byte discrete -termOnClient -nodes 5 -d -follower Uniform-150-300 -candidate Uniform-150-300 -leader Fixed-75 -delay Normal-7-3
-./cmdstart.byte discrete -termOnClient -nodes 5 -d -follower Uniform-150-300 -candidate Uniform-150-300 -leader Fixed-75 -delay Normal-7-3 -cmds 50 -loss 0.1
-./cmdstart.byte discrete -termOnClient -nodes 10 -d -follower Uniform-150-300 -candidate Uniform-150-300 -leader Fixed-75 -delay Normal-7-3 -cmds 50 -failure Uniform-0-1000 -recover Uniform-5-10
+./cmdstart.byte discrete -termOnClient -nodes 5 -follower Uniform-150-300 -candidate Uniform-150-300 -leader Fixed-75 -delay Normal-7-3
+./cmdstart.byte discrete -termOnClient -nodes 5 -follower Uniform-150-300 -candidate Uniform-150-300 -leader Fixed-75 -delay Normal-7-3 -cmds 50 -loss $LOSS
+./cmdstart.byte discrete -termOnClient -nodes 10 -follower Uniform-150-300 -candidate Uniform-150-300 -leader Fixed-75 -delay Normal-7-3 -cmds 50 -failure Uniform-0-1000 -recover Uniform-5-10
 
 
 # unit testing
