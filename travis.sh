a960a285beb8ea8209c32ec2d88c748eee8d3808
Heidi-ann <hh360@cam.ac.uk>
2014-11-15 22:33:19 +0000

    updating to OCaml 4.02.0

--- travis.sh
+++ travis.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-ppa=avsm/ocaml41+opam11
+ppa=avsm/ocaml42+opam12
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam 
