41a7eb97c30058a79ffd1f7a807354bb63a86679
Heidi-ann <hh360@cam.ac.uk>
2014-11-16 10:46:50 +0000

    adding older ocaml/opam version to travis test matrix

--- travis.sh
+++ travis.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
-ppa=avsm/ocaml42+opam12
+
+case "$OCAML_VERSION,$OPAM_VERSION" in
+4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
+4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
+4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
+4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
+4.01.0,1.2.0) ppa=avsm/ocaml41+opam12 ;;
+4.02.0,1.1.0) ppa=avsm/ocaml42+opam11 ;;
+4.02.0,1.2.0) ppa=avsm/ocaml42+opam12 ;;
+*) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
+esac
+
+
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam 
