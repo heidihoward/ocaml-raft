b118d111e514bab9460a1737e319b025242ae472
Heidi-ann <hh360@cam.ac.uk>
2014-11-21 23:35:57 +0000

    trying core_kernel_js

--- js/build.sh
+++ js/build.sh
@@ -1,4 +1,4 @@
-ocamlfind ocamlc -package js_of_ocaml -package core_kernel -package js_of_ocaml.syntax \
+ocamlfind ocamlc -package js_of_ocaml -package core_kernel_js -package js_of_ocaml.syntax \
 	-syntax camlp4o -linkpkg -g -o ocamlraft.byte ocamlraft.ml
 
-js_of_ocaml --pretty --noinline ocamlraft.byte
+js_of_ocaml --pretty --noinline --disable genprim ocamlraft.byte
--- js/ocamlraft.ml
+++ js/ocamlraft.ml
@@ -1,6 +1,6 @@
-open Core_kernel.Std
+open Core_kernel_js.Std
 
 let run () = 
-	if Int.is_positive 10 then 3 else 2
+	if Int.is_positive 4 then 3 else 2
 
 let _ = Js.Unsafe.global##run <- Js.wrap_callback run;
